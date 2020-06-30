0e912a565c343567f32a2e69523a03711a56c5f5
Bobby Schade <bschade.18@gmail.com>
2020-06-30 09:36:19 -0500

    adding smooth scroll back

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
 // Sticky menu background
 window.addEventListener('scroll', function () {
   if (window.scrollY > 150) {
@@ -6,19 +22,3 @@ window.addEventListener('scroll', function () {
     document.querySelector('#navbar').style.opacity = 1;
   }
 });
-
-// // Smooth Scrolling
-// $('#navbar a, .btn').on('click', function (event) {
-//   if (this.hash !== '') {
-//     event.preventDefault();
-
-//     const hash = this.hash;
-
-//     $('html, body').animate(
-//       {
-//         scrollTop: $(hash).offset().top - 100,
-//       },
-//       800
-//     );
-//   }
-// });
